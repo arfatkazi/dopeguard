5c944c7aaee48c27debd29573c4a2a252fed5442
arfat <arfatkazi115@gmail.com>
2025-12-05 13:52:24 +0530

    fix extension

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: "DopeGuard",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
